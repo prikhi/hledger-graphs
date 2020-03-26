94cec62de869a14987893d3601706dd6fbf1520f
Pavan Rikhi <pavan.rikhi@gmail.com>
2020-03-26 04:05:05 -0400

    Add Monthly Income/Expenses Bar Graph
    Add a function for easily generating the monthly balance report & totals
    for an hledger query.
    
    Add a function for transforming the monthly totals for an account into
    a multi-bar graph of the monthly Income & Expense totals.
    
    Modify the application to render the bar graph below the separate income
    & expense pie charts.

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,9 @@ import           Graphics.Rendering.Chart.Backend.Diagrams
 import           Graphics.Rendering.Chart.Backend.Types
 import           Graphics.Rendering.Chart.Renderable
 import           Graphics.Rendering.Chart.State
+import           Hledger.Data.Journal           ( journalExpenseAccountQuery
+                                                , journalRevenueAccountQuery
+                                                )
 
 import           Lib
 
@@ -13,8 +16,17 @@ main :: IO ()
 main = do
     journal <- getJournal
     dEnv    <- defaultEnv vectorAlignmentFns 500 500
-    let makeDiagram = fst . runBackendR dEnv . toRenderable . execEC
-    let expensesPie = makeDiagram $ expensePieGraph $ getExpenseReport journal
-        incomePie   = makeDiagram $ incomePieGraph $ getIncomeReport journal
+    dEnv_   <- defaultEnv vectorAlignmentFns 1000 500
+    let makeDiagram  = fst . runBackendR dEnv . toRenderable . execEC
+        makeDiagram_ = fst . runBackendR dEnv_ . toRenderable . execEC
+        expensesPie  = makeDiagram $ expensePieGraph $ getExpenseReport journal
+        incomePie    = makeDiagram $ incomePieGraph $ getIncomeReport journal
+        bars         = makeDiagram_ $ incomeExpensesBars
+            (getMonthlyBalances journalRevenueAccountQuery journal)
+            (getMonthlyBalances journalExpenseAccountQuery journal)
     renderSVG "income-expense-pie.svg" (mkSizeSpec $ V2 (Just 1010) Nothing)
-        $ hcat [expensesPie, strutX 10, incomePie]
+        $ vcat
+              [ centerX $ hcat [expensesPie, strutX 10, incomePie]
+              , strutY 10
+              , centerX $ bars
+              ]
--- src/Lib.hs
+++ src/Lib.hs
@@ -13,21 +13,41 @@ import           Graphics.Rendering.Chart.Easy  ( (.~)
                                                 , pie_data
                                                 , pitem_value
                                                 , pitem_label
+                                                , Layout
+                                                , EC
+                                                , PlotIndex
+                                                , layout_title
+                                                , layout_x_axis
+                                                , laxis_generate
+                                                , autoIndexAxis
+                                                , plot
+                                                , plotBars
+                                                , bars
+                                                , addIndexes
                                                 )
 import           Hledger.Data.Amount            ( amounts
                                                 , showMixedAmount
                                                 )
+import           Hledger.Data.Dates             ( showDateSpan )
 import           Hledger.Data.Journal           ( journalExpenseAccountQuery
                                                 , journalRevenueAccountQuery
                                                 )
 import           Hledger.Data.Types             ( Journal
                                                 , Amount(aquantity)
+                                                , Interval(Months)
+                                                , DateSpan
+                                                , MixedAmount
                                                 )
 import           Hledger.Query                  ( Query )
 import           Hledger.Read                   ( defaultJournal )
-import           Hledger.Reports.ReportOptions  ( ReportOpts(depth_) )
+import           Hledger.Reports.ReportOptions  ( ReportOpts(depth_, interval_)
+                                                )
 import           Hledger.Reports.BalanceReport  ( BalanceReportItem
                                                 , balanceReport
+                                                )
+import           Hledger.Reports.MultiBalanceReport
+                                                ( MultiBalanceReport(..)
+                                                , multiBalanceReport
                                                 )
 
 import qualified Data.Text                     as T
@@ -48,6 +68,19 @@ getIncomeReport = getBalanceReport journalRevenueAccountQuery
 getBalanceReport :: (Journal -> Query) -> Journal -> [BalanceReportItem]
 getBalanceReport query j =
     fst $ balanceReport (def { depth_ = Just 1 }) (query j) j
+
+getMonthlyBalanceReport :: (Journal -> Query) -> Journal -> MultiBalanceReport
+getMonthlyBalanceReport query j = multiBalanceReport
+    (def { interval_ = Months 1, depth_ = Just 0 })
+    (query j)
+    j
+
+getMonthlyBalances :: (Journal -> Query) -> Journal -> [(DateSpan, MixedAmount)]
+getMonthlyBalances query j =
+    let (MultiBalanceReport (dates, _, totals)) =
+                getMonthlyBalanceReport query j
+    in  zip dates $ (\(amts, _, _) -> amts) totals
+
 
 
 -- GRAPHS
@@ -72,3 +105,22 @@ balanceReportPieGraph title depth items = do
         def
             & (pitem_value .~ realToFrac (aquantity $ head $ amounts amts))
             & (pitem_label .~ (T.unpack name ++ " " ++ showMixedAmount amts))
+
+
+incomeExpensesBars
+    :: [(DateSpan, MixedAmount)]
+    -> [(DateSpan, MixedAmount)]
+    -> EC (Layout PlotIndex Double) ()
+incomeExpensesBars income expenses = do
+    layout_title .= "Income & Expenses"
+    layout_x_axis . laxis_generate .= autoIndexAxis (map fst values)
+    plot $ plotBars <$> bars titles (addIndexes $ map snd values)
+  where
+    titles = ["Income", "Expenses"]
+    values = zipWith
+        (\(d, i) (_, e) -> (showDateSpan d, [mixedToFrac i, mixedToFrac e]))
+        income
+        expenses
+
+mixedToFrac :: Fractional a => MixedAmount -> a
+mixedToFrac = abs . realToFrac . aquantity . head . amounts

07e4aab48b475383404c353b74f79b434c893286
Pavan Rikhi <pavan.rikhi@gmail.com>
2020-03-26 03:10:57 -0400

    Add Income Pie Chart
    Refactor the library to ease running of balance reports with arbitrary
    queries and generating pie charts from balance reports.
    
    Add a getIncomeReport and incomePieGraph function for rendering the
    income accounts as a pie chart.
    
    Change the application to render the expense & income pie charts
    side-by-side, using the diagrams library.

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
-import           Data.Default                   ( def )
+import           Diagrams.Prelude
+import           Diagrams.Backend.SVG
 import           Graphics.Rendering.Chart.Backend.Diagrams
-                                                ( toFile )
+import           Graphics.Rendering.Chart.Backend.Types
+import           Graphics.Rendering.Chart.Renderable
+import           Graphics.Rendering.Chart.State
 
 import           Lib
 
 main :: IO ()
 main = do
-    report <- getExpenseReport
-    toFile def "expense-pie.svg" $ expensePieGraph report
+    journal <- getJournal
+    dEnv    <- defaultEnv vectorAlignmentFns 500 500
+    let makeDiagram = fst . runBackendR dEnv . toRenderable . execEC
+    let expensesPie = makeDiagram $ expensePieGraph $ getExpenseReport journal
+        incomePie   = makeDiagram $ incomePieGraph $ getIncomeReport journal
+    renderSVG "income-expense-pie.svg" (mkSizeSpec $ V2 (Just 1010) Nothing)
+        $ hcat [expensesPie, strutX 10, incomePie]
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,17 +5,25 @@ import           Control.Monad.State            ( MonadState )
 import           Data.Default                   ( def )
 import           Graphics.Rendering.Chart.Easy  ( (.~)
                                                 , (.=)
+                                                , (&)
                                                 , PieLayout
+                                                , PieItem
                                                 , pie_title
                                                 , pie_plot
                                                 , pie_data
                                                 , pitem_value
                                                 , pitem_label
-                                                , pitem_offset
                                                 )
-import           Hledger.Data.Amount            ( amounts )
-import           Hledger.Data.Journal           ( journalExpenseAccountQuery )
-import           Hledger.Data.Types             ( Amount(aquantity) )
+import           Hledger.Data.Amount            ( amounts
+                                                , showMixedAmount
+                                                )
+import           Hledger.Data.Journal           ( journalExpenseAccountQuery
+                                                , journalRevenueAccountQuery
+                                                )
+import           Hledger.Data.Types             ( Journal
+                                                , Amount(aquantity)
+                                                )
+import           Hledger.Query                  ( Query )
 import           Hledger.Read                   ( defaultJournal )
 import           Hledger.Reports.ReportOptions  ( ReportOpts(depth_) )
 import           Hledger.Reports.BalanceReport  ( BalanceReportItem
@@ -25,21 +33,42 @@ import           Hledger.Reports.BalanceReport  ( BalanceReportItem
 import qualified Data.Text                     as T
 
 
-getExpenseReport :: IO [BalanceReportItem]
-getExpenseReport = do
-    journal <- defaultJournal
-    return . fst $ balanceReport (def { depth_ = Just 2 })
-                                 (journalExpenseAccountQuery journal)
-                                 journal
+-- JOURNAL
 
+getJournal :: IO Journal
+getJournal = defaultJournal
+
+getExpenseReport :: Journal -> [BalanceReportItem]
+getExpenseReport = getBalanceReport journalExpenseAccountQuery
+
+getIncomeReport :: Journal -> [BalanceReportItem]
+getIncomeReport = getBalanceReport journalRevenueAccountQuery
+
+
+getBalanceReport :: (Journal -> Query) -> Journal -> [BalanceReportItem]
+getBalanceReport query j =
+    fst $ balanceReport (def { depth_ = Just 1 }) (query j) j
+
+
+-- GRAPHS
 
 expensePieGraph :: MonadState PieLayout m => [BalanceReportItem] -> m ()
-expensePieGraph items = do
-    pie_title .= "Expenses"
-    pie_plot . pie_data .= map pieItem (filter (\(_, _, d, _) -> d == 1) items)
+expensePieGraph = balanceReportPieGraph "Expenses" 1
+
+
+incomePieGraph :: MonadState PieLayout m => [BalanceReportItem] -> m ()
+incomePieGraph = balanceReportPieGraph "Income" 1
+
+
+balanceReportPieGraph
+    :: MonadState PieLayout m => String -> Int -> [BalanceReportItem] -> m ()
+balanceReportPieGraph title depth items = do
+    pie_title .= title
+    pie_plot . pie_data .= map toPieItem
+                               (filter (\(_, _, d, _) -> d == depth) items)
   where
-    pieItem (_, name, _, amts) =
-        (pitem_value .~ realToFrac (aquantity $ head $ amounts amts))
-            $ (pitem_label .~ (T.unpack name))
-            $ (pitem_offset .~ 0)
-            $ def
+    toPieItem :: BalanceReportItem -> PieItem
+    toPieItem (_, name, _, amts) =
+        def
+            & (pitem_value .~ realToFrac (aquantity $ head $ amounts amts))
+            & (pitem_label .~ (T.unpack name ++ " " ++ showMixedAmount amts))
